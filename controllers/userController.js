a74e515dc91e752492b08183663db202d6cfd165
Orangesmw <106233500+Orangesmw1@users.noreply.github.com>
2023-05-17 18:37:44 +0700

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,6 @@ const userController = {
   getAllUser: async (req, res) => {
     try {
       const users = await User.find();
-
       res.status(200).json(users);
     } catch (err) {
       res.status(500).json(err);
